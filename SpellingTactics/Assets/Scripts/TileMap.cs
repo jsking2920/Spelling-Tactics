bdbcea2c21c0d22fbf6c14fe970d3c195fa424ab
Scott King <jsking@andrew.cmu.edu>
2024-07-22 23:11:48 -0400

    Finish tile prefabs

--- SpellingTactics/Assets/Scripts/TileMap.cs
+++ SpellingTactics/Assets/Scripts/TileMap.cs
@@ -20,11 +20,46 @@ public class TileMap : MonoBehaviour
     {
         tiles = new int[10, 10];
 
+        // Initialize every tile as grass
         for (int x = 0; x < mapSizeX; x++)
         {
             for (int y = 0; y < mapSizeY; y++)
             {
-                tiles[x, y] = 0;
+                tiles[x, y] = 1;
+            }
+        }
+
+        // TODO: set up a system to build maps from text files
+        tiles[0, 0] = 0;
+        tiles[0, 1] = 0;
+        tiles[0, 2] = 0;
+        tiles[0, 4] = 0;
+        tiles[1, 4] = 0;
+        tiles[0, 5] = 0;
+        tiles[1, 4] = 0;
+        tiles[0, 6] = 0;
+        tiles[1, 6] = 0;
+        tiles[0, 7] = 0;
+
+        tiles[4, 0] = 3;
+        tiles[4, 1] = 3;
+        tiles[4, 2] = 3;
+        tiles[4, 3] = 3;
+        tiles[4, 4] = 3;
+        tiles[4, 5] = 3;
+        tiles[4, 6] = 3;
+        tiles[4, 7] = 3;
+
+        tiles[7, 7] = 2;
+        tiles[8, 7] = 2;
+        tiles[7, 8] = 2;
+        tiles[8, 8] = 2;
+
+        // Initialize every tile as empty
+        for (int x = 0; x < mapSizeX; x++)
+        {
+            for (int y = 0; y < mapSizeY; y++)
+            {
                 Instantiate(tileTypes[tiles[x, y]].tilePrefab, new Vector3(x, 0, y), Quaternion.identity, transform);
             }
         }
